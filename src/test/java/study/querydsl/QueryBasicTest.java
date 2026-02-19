bc1229984cc7ebbcaa51bb51be33782de008d618
jaehyucklim <jhlim087@gmail.com>
2026-02-19 15:37:08 +0900

    feat: add join with on filtering and non-relation left join

--- src/test/java/study/querydsl/QueryBasicTest.java
+++ src/test/java/study/querydsl/QueryBasicTest.java
@@ -240,4 +240,45 @@ public class QueryBasicTest {
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    /**
+     * 회원과 팀을 join 하면서, 팀 이름이 teamA인 팀만 조회(회원은 모두 조회)
+     * JPQL: select m, t from Member m left join m.team t on t.name = 'teamA'
+     */
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .join(member.team, team)
+//                .on(team.name.eq("teamA"))
+                .where(team.name.eq("teamA")) // inner join은 on절과 where에서 같은 결과
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관관계 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+     */
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team) // id matching X -> 연관관계 없는 엔티티를 ON 절로 외부 조인
+                .on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
