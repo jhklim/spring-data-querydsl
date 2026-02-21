fb5c8eb539d455d51aa5454470021ded8bebfafe
jaehyucklim <jhlim087@gmail.com>
2026-02-21 11:56:48 +0900

    refactor: inject JPAQueryFactory as Spring Bean

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -2,6 +2,7 @@ package study.querydsl.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -12,15 +13,11 @@ import java.util.Optional;
 import static study.querydsl.entity.QMember.*;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberJpaRepository {
 
     private final EntityManager em;
     private final JPAQueryFactory queryFactory;
-
-    public MemberJpaRepository(EntityManager em) {
-        this.em = em;
-        this.queryFactory = new JPAQueryFactory(em);
-    }
 
     public void save(Member member) {
         em.persist(member);
